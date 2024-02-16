c4623b039fbc85832a3ca39bc88845ab67f177d4
Ta-Yi Cheng <admin@tranquility.tech>
2024-02-16 23:56:16 +0800

    style: eslint format

--- server/index.mjs
+++ server/index.mjs
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express from 'express'
+import express from "express";
 import fetch from "node-fetch";
 
 const { KET, TOKEN } = process.env;
@@ -7,42 +7,42 @@ const FIDO2_SERVER = ""; // https://admin-auth.ofido.tw
 const app = express();
 
 app.post("/register", async (req, res) => {
-  try {
-    const { email } = req.body;
+	try {
+		const { email } = req.body;
 
-    const { data: user } = await fetch(`${FIDO2_SERVER}/api/tenant/${KEY}/user`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${TOKEN}`
-      },
-      body: JSON.stringify({
-        email,
-        name: email
-      })
-    }).then((res) => res.json());
+		const { data: user } = await fetch(`${FIDO2_SERVER}/api/tenant/${KEY}/user`, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${TOKEN}`
+			},
+			body: JSON.stringify({
+				email,
+				name: email
+			})
+		}).then((res) => res.json());
 
-    const { data: token } = await fetch(`${FIDO2_SERVER}/api/tenant/${KEY}/user/${user.id}/token`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${TOKEN}`
-      }
-    }).then((res) => res.json());
+		const { data: token } = await fetch(`${FIDO2_SERVER}/api/tenant/${KEY}/user/${user.id}/token`, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${TOKEN}`
+			}
+		}).then((res) => res.json());
 
-    res.json({
-      token
-    });
-  } catch (err) {
-    res.status(503).json({ err });
-  }
+		res.json({
+			token
+		});
+	} catch (err) {
+		res.status(503).json({ err });
+	}
 });
 
 const server = app.listen(8081, function () {
-  const host = server.address().address
-  const port = server.address().port
+	const host = server.address().address;
+	const port = server.address().port;
 
-   console.log("Example app listening at http://%s:%s", host, port)
+	console.log("Example app listening at http://%s:%s", host, port);
 });
